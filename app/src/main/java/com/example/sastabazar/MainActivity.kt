f4010054a774af3c764214e41bb1c2e8f11e3adc
naveenkanojiya <naveenkanojiya9956@gmail.com>
2023-10-17 23:53:36 +0530

    This is fourth Commit

--- app/src/main/java/com/example/sastabazar/MainActivity.kt
+++ app/src/main/java/com/example/sastabazar/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun GotoNext(view: View) {
-        startActivity(Intent(this@MainActivity,SingUpLoginActivity::class.java))
-        finish()
+        if (Firebase.auth==null){
+            startActivity(Intent(this@MainActivity,SingUpLoginActivity::class.java))
+            finish()
+        }else{
+            startActivity(Intent(this@MainActivity,HomeActivity::class.java))
+            finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/sastabazar/SingUpLoginActivity.kt
+++ app/src/main/java/com/example/sastabazar/SingUpLoginActivity.kt
@@ -1,10 +1,12 @@
 package com.example.sastabazar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.sastabazar.databinding.ActivitySingUpLoginBinding
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class SingUpLoginActivity : AppCompatActivity() {
@@ -21,8 +23,23 @@ class SingUpLoginActivity : AppCompatActivity() {
                 binding.pass.text.toString()
             ).addOnCompleteListener {
                 if (it.isSuccessful){
-                    Toast.makeText(this@SingUpLoginActivity,"User Created",Toast.LENGTH_LONG)
-                        .show()
+
+                    var userModel = UserModel(
+                        binding.firstName.text.toString(),
+                        binding.lastName.text.toString(),
+                        binding.email.text.toString(),
+                        binding.pass.text.toString(),
+                    )
+
+                    Firebase.database.reference.child("Users").child(it.result.user!!.uid)
+                        .setValue(userModel).addOnSuccessListener {
+                            startActivity(Intent(this@SingUpLoginActivity,HomeActivity::class.java))
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this@SingUpLoginActivity,it.localizedMessage,Toast.LENGTH_LONG)
+                                .show()
+                        }
+
                 }else{
                     Toast.makeText(
                         this@SingUpLoginActivity,
